f82a0ab070e21b8ab9968edeb1aae6973b0885f5
FluYVy <florian.reichart206@gmail.com>
2024-12-21 16:49:48 +0100

    Reworked MAX30101-Library
    Changes:
    - Added better Doxygen comments

--- Software/Libraries/Modules/MAX30101/max30101.h
+++ Software/Libraries/Modules/MAX30101/max30101.h
@@ -10,21 +10,26 @@
 
 #include "i2c_master.h"
 
+
 // I2C address for the MAX30101 sensor
+
+/**
+ * @def MAX30101_I2C_ADDRESS
+ * 
+ * I2C address for the MAX30101 sensor.
+ * The default value is 0x55, which can be modified to other addresses if needed.
+*/
 #define MAX30101_I2C_ADDRESS 0x55
 
 /**
 * @brief Starts measurements on the MAX30101 sensor.
 *
-* This function configures the sensor to begin taking measurements.
-* It sets the output mode, enables the Automatic Gain Control (AGC),
-* the Analog Front End (AFE), and activates the algorithm for processing
-* the data.
+* This function starts the measurement process for the MAX30101 sensor.
 */
 void max30101_start_measurements();
 
 /**
-* @brief Performs a single measurement with the MAX30101 sensor.
+* @brief Perform a single measurement
 *
 * This function reads measurement data from the MAX30101 sensor and
 * stores it in the provided array. The length parameter determines
@@ -33,6 +38,11 @@ void max30101_start_measurements();
 * @param[out] values Array to store the measurement values.
 *                    Should have at least the length specified.
 * @param[in] length The number of values to read (1-3).
+* 0 ... no values are returned
+* 1 ... measure Heart rate
+* 2 ... measure Heart Rate and SpO2
+* 3 ... measure Heart Rate, SpO2 and Interbeat interval (IBI)
+*
 */
 void max30101_single_measurement(uint16_t values[], uint8_t length);
 
